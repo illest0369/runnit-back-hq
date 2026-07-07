9aec2b8bf56b46320079cc7fa9a1e5b0732a6d3c
illest0369 <illest0369@users.noreply.github.com>
2026-07-06 19:23:23 -0700

    Fix sources route shell

---
+++ app/sources/page.tsx
@@ -0,0 +1,8 @@
+import OperatorApp from "@/components/rbhq/OperatorApp";
+
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
+export default function SourcesPage() {
+  return <OperatorApp initialTab="sources" />;
+}
